d98f0d7d47531236b8b74fb53e3dc8a1c1a5822a
Pavel Sobolev <Paveloom@mail.ru>
2020-03-01 10:55:22 +0000

    Тест с получением полного пути.

--- .github/scripts/Build&Clean (macOS, установка и проверка gmake).sh
+++ .github/scripts/Build&Clean (macOS, установка и проверка gmake).sh
@@ -10,7 +10,24 @@ curl -O https://github.com/macports/macports-base/releases/download/v2.6.2/MacPo
 printf "\n Выполняется получение полного пути к файлу.\n\n"
 
 
-ls -l
+TARGET_FILE="MacPorts-2.6.2-10.15-Catalina.pkg"
+
+cd `dirname $TARGET_FILE`
+TARGET_FILE=`basename $TARGET_FILE`
+
+# Iterate down a (possible) chain of symlinks
+while [ -L "$TARGET_FILE" ]
+do
+    TARGET_FILE=`readlink $TARGET_FILE`
+    cd `dirname $TARGET_FILE`
+    TARGET_FILE=`basename $TARGET_FILE`
+done
+
+# Compute the canonicalized name by finding the physical path 
+# for the directory we're in and appending the target file.
+PHYS_DIR=`pwd -P`
+RESULT=$PHYS_DIR/$TARGET_FILE
+echo $RESULT
 
 # Установка пакета
 
