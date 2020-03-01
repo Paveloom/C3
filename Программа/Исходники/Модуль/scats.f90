01d26f66c4a4980275411bd79e352bc72d87e712
Pavel Sobolev <Paveloom@mail.ru>
2020-03-01 18:41:02 +0000

    SCATS теперь в нижнем регистре.

--- Программа/Исходники/Модуль/scats.f90
+++ Программа/Исходники/Модуль/scats.f90
@@ -1,15 +1,15 @@
-module SCATS ! Модуль, содержащий API алгоритма СКАВР
+module scats ! Модуль, содержащий API алгоритма СКАВР
 use gen_params_m, only : gen_params_type ! Тип, определяющий параметры 
                                          ! для генерации временного ряда
 implicit none
      
      private
-     public :: SCATS_API ! API модуля
+     public :: scats_api ! API модуля
      
-     type SCATS_API
+     type scats_api
 
           type( gen_params_type ) gen ! Параметры для генерации временного ряда
 
-     end type SCATS_API
+     end type scats_api
      
-end module SCATS
+end module scats
