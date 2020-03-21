b841ef07c05b724546b82d74d5f51ee2e7638a02
Pavel Sobolev <Paveloom@mail.ru>
2020-03-21 07:46:34 +0000

    Упрощены скрипты визуализации.

--- Программа/Исходники/SCATS/Визуализация/Результат/scats_visualize_result_s.py
+++ Программа/Исходники/SCATS/Визуализация/Результат/scats_visualize_result_s.py
@@ -20,30 +20,28 @@ from sys import argv
 
 # Сохранение переданных аргументов
 input_name = str(argv[1])
+stage = str(argv[2])
+output_name = str(argv[3])
+title = str(argv[4])
 
-if str(argv[3]) == '-0-':
+if output_name == '-0-':
     custom_path = False
-    if str(argv[2]) == 'no_trend':
+    if stage == 'no_trend':
         output_name = 'no_trend'
-    if str(argv[2]) == 'periodogram':
+    if stage == 'periodogram':
         output_name = 'periodogram'
 else:
     custom_path = True
-    output_name = str(argv[3])
 
-if str(argv[4]) == '-0-':
-    if str(argv[2]) == 'no_trend':
+if title == '-0-':
+    if stage == 'no_trend':
         title = 'После извлечения тренда'
-        stage = 'no_trend'
         xlabel = 'Время'
         ylabel = 'Значение'
-    elif str(argv[2]) == 'periodogram':
-        stage = 'periodogram'
+    elif stage == 'periodogram':
         title = 'Периодограмма'
         xlabel = 'Частота'
         ylabel = 'Значение'
-else:
-    title = str(argv[4])
 
 # Настройки графиков
 
@@ -70,26 +68,31 @@ rcP["legend.fontsize"] = 12
 ## Создание пустого списка
 lines = []
 
-## Считывание строк с данными
-with open(input_name) as f:
-    for line in islice(f, 1, 17, 3):
-        lines.append(line)
-
 if stage == 'no_trend': 
 
+    ## Считывание строк с данными
+    with open(input_name) as f:
+        for line in islice(f, 7, 11, 3):
+            lines.append(line)
+
     ## Получение значений массива времени
-    x = np.array(lines[2].split(), dtype = np.float)
+    x = np.array(lines[0].split(), dtype = np.float)
 
     ## Получение значений массива значений
-    y = np.array(lines[3].split(), dtype = np.float)
+    y = np.array(lines[1].split(), dtype = np.float)
 
 elif stage == 'periodogram':
 
+    ## Считывание строк с данными
+    with open(input_name) as f:
+        for line in islice(f, 13, 17, 3):
+            lines.append(line)
+
     ## Получение значений массива частот периодограммы
-    x = np.array(lines[4].split(), dtype = np.float)
+    x = np.array(lines[0].split(), dtype = np.float)
 
     ## Получение значений массива значений периодограммы
-    y = np.array(lines[5].split(), dtype = np.float) 
+    y = np.array(lines[1].split(), dtype = np.float) 
 
 # Создание и сохранение фигуры
 
